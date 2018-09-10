bf905274803cc8a284e267c24cb1a062a43c9292
AgungEdiKuncoro <42725305+AgungEdiKuncoro@users.noreply.github.com>
2018-09-10 09:59:37 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,46 @@
-<?php 
-//Tuliskan kode anda di bawah ini
+<?php
+// error_reporting(0);
+$angka = $_GET['angka'];
+$genap = array();
+$ganjil = array();
+function piramid($x, $y){
+	if ($x > 0) {
+		if ($y > 0) {
+			echo "* ";
+			$y--;
+			piramid($x, $y);
+		}
+		else{
+			echo "<br>";
+			$x--;
+			piramid($x, $x);
+		}
+	}
+}
+piramid ($angka, $angka);
 
+echo "<br>";
+for ($i = 1; $i <= $angka ; $i++) { 
+    if ($i % 2 == 0) {
+        array_push($genap, $i);
+    }else {
+        array_push($ganjil, $i);
+    }
+}   
+
+echo "Genap : ";
+for ($i = 0; $i < count($genap) ; $i++) { 
+    echo $genap[$i];
+}
+echo "<br>";
+echo "Ganjil : ";
+for ($i = 0; $i < count($ganjil) ; $i++) { 
+    echo $ganjil[$i];
+}
+
+echo "<br>";
+
+echo "Jumlah deret genap : ".count($genap);
+echo "<br>";
+echo "Jumlah deret ganjil : ".count($ganjil);
 ?>
